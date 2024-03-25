ce9950e37ab21940989308d39909a3dc491ba869
MartinWachira <67173409+mmwachira@users.noreply.github.com>
2024-03-25 10:37:31 +0200

    feat: update PropDrilling.tsx
    Use of different components to hold the prop and the 4th component using the prop

--- src/components/PropDrilling.tsx
+++ src/components/PropDrilling.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState } from "react";
-//import Audi from "./Audi";
+import Audi from "./Audi";
+import Mercedes from "./Mercedes";
+import BMW from "./BMW";
 
 const PropDrilling = () => {
   const [car, setCar] = useState("Coupe");
@@ -8,17 +10,23 @@ const PropDrilling = () => {
   return <Audi car={car} />;
 };
 
+<>
+<Audi car={""} />
+<Mercedes car={""} />
+<BMW car="" />
+</>
 
-const Audi = ({ car }: CarProps) => {
-  return <Mercedes car={car} />;
-};
 
-const Mercedes = ({ car }: CarProps) => {
-  return <BMW car={car} />;
-};
+// const Audi = ({ car }: CarProps) => {
+//   return <Mercedes car={car} />;
+// };
 
-const BMW = ({ car }: CarProps) => {
-  return <div>{car}</div>;
-};
+// const Mercedes = ({ car }: CarProps) => {
+//   return <BMW car={car} />;
+// };
+
+// const BMW = ({ car }: CarProps) => {
+//   return <div>{car}</div>;
+// };
 
 export default PropDrilling;
